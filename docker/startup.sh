1dbd41cbe3bca85b2c22a07445f796c61e5056ad
Arthur Cadore <74879952+arthurcadore@users.noreply.github.com>
2024-03-27 08:11:10 -0300

    :bug: removed sudo command from startup

--- docker/startup.sh
+++ docker/startup.sh
@@ -6,8 +6,8 @@ username="capacita"
 password="1234567890"
 
 # Create user/passwd for access the container: 
-sudo useradd -m -s /bin/bash "$username"
-echo "$username:$password" | sudo chpasswd
+useradd -m -s /bin/bash "$username"
+echo "$username:$password" | chpasswd
 
 # starts pulse audio service: 
 /usr/bin/pulseaudio
